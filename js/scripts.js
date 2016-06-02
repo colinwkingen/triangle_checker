e642651d0476c7c52533f7195b946f549ff933c2
Colin W Kingen and Wolfgang Warneke <wolfgang@>
2016-06-02 14:37:52 -0700

    add triangle differentiation logic to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
-  $("submit-button").submit(function(event) {
-
-
-
+  $("#triangles").submit(function(event) {
+    var sideOne = parseInt($("#side1").val());
+    var sideTwo = parseInt($("#side2").val());
+    var sideThree = parseInt($("#side3").val());
+    if ((sideOne+sideTwo<=sideThree)||(sideTwo+sideThree<=sideOne)||(sideOne+sideThree<=sideTwo)) {
+      $("#result h1").text('This is not a triangle.');
+    } else if (sideOne === sideTwo && sideOne === sideThree) {
+      $("#result h1").text('This is an equilateral.');
+    } else if (sideOne === sideTwo || sideTwo === sideThree || sideOne === sideThree) {
+      $("#result h1").text('This is an isosceles triangle.');
+    } else {
+      $("#result h1").text('This is a scalene triangle.');
+    };
     event.preventDefault();
   });
 });
